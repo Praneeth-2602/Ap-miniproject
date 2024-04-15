b878138abc5bb56e0e87fb8a77abe52975c9f032
Praneeth Palugula <praneeth.palugula@gmail.com>
2024-04-16 01:45:04 +0530

    m

--- public/script5.js
+++ public/script5.js
@@ -1,9 +1,24 @@
 // Check if localStorage.isLoggedIn is truthy
-if (localStorage.isLoggedIn) {
+if (localStorage.isLoggedIn == 'true') {
     // If it has a value, hide login and signup buttons
     document.getElementById('login').style.display = 'none';
     document.getElementById('signup').style.display = 'none';
     
     // Show signout button
     document.getElementById('signout').style.display = 'contents';
+}
+else if (localStorage.isLoggedIn == 'false'){
+    // Show the login and signup buttons
+    document.getElementById('login').style.display = 'contents';
+    document.getElementById('signup').style.display = 'contents';
+    
+    // Hide the signout button
+    document.getElementById('signout').style.display = 'none';
+}
+
+// Function to handle signout
+function handleSignout() {
+    // Set isLoggedIn to false in localStorage
+    localStorage.isLoggedIn = false;
+    localStorage.removeItem('username');
 }
